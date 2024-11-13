1f6ab75245c38fa9b48d34c4808dcfd2ccd0b86f
balogh_b <111227723+baloghb0110@users.noreply.github.com>
2024-11-13 15:25:23 +0100

    implement event loader in bot.js

--- bot.js
+++ bot.js
@@ -14,4 +14,15 @@ const client = new Client({
 
 client.commands = new Collection();
 
+// Load events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(client, ...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(client, ...args));
+  }
+}
+
 client.login(config.token);
